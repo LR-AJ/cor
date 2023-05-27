6023c1275f08e2eb03d85af74f067037c1232ede
LR-AJ <legalraastatech3@gmail.com>
2023-05-27 16:05:26 +0530

    Update in service

--- service.php
+++ service.php
@@ -1,69 +1,19 @@
 <?php
 
-$search = $_GET['search'];
+$search = $_GET['searchKey'];
+$domain = array("LLP Registration"=>"https://companyregistrationonline.in/llp-registration/", "Private Limited Company Registration"=>"https://companyregistrationonline.in/private-limited-company-registration/", "Nidhi Company Registeration"=>"https://companyregistrationonline.in/nidhi-company-registration/");
+// $list = array('LLP Registration','NBFC License','Nidhi Company Registeration','Private Limited Company Registration','Section 8 Company Registration','OPC Registration');
+$list = ['LLP Registration','NBFC License','Nidhi Company Registeration','Private Limited Company Registration','Section 8 Company Registration','OPC Registration'];
+if(in_array($search,$list)){
+  echo("check");
+  // header('Location:'.'https://companyregistrationonline.in/'. $domain[$search]);
+  header("Location: https://www.legalraasta.com/thanks/");
 
-$xmlDoc=new DOMDocument();
-$xmlDoc->load("links.xml");
-//lookup all links from the xml file if length of search>0
-if (strlen($search)>0) {
-    $hint="";
-    for($i=0; $i<($x->length); $i++) {
-      $y=$x->item($i)->getElementsByTagName('title');
-      $z=$x->item($i)->getElementsByTagName('url');
-      if ($y->item(0)->nodeType==1) {
-        //find a link matching the search text
-        if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$search)) {
-          if ($hint=="") {
-            $hint="<a href='" .
-            $z->item(0)->childNodes->is->item(0)->nodeValue . "</a>";
-          } elstem(0)->nodeValue .
-            "' target='_blank'>" .
-            $y->item(0)->childNodee {
-            $hint=$hint . "<br /><a href='" .
-            $z->item(0)->childNodes->item(0)->nodeValue .
-            "' target='_blank'>" .
-            $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
-          }
-        }
-      }
-    }
-  }
+}
+
   
   // Set output to "no suggestion" if no hint was found
   // or to the correct values
-  if ($hint=="") {
-    $response="no suggestion";
-  } else {
-    $response=$hint;
-  }
-  
-  //output the response
-  echo $response;
+
   ?>
 
-// $servername = "localhost";
-// $username = "bob";
-// $password = "123456";
-// $db = "classDB";
-
-// $conn = new mysqli($servername, $username, $password, $db);
-
-// if ($conn->connect_error){
-// 	die("Connection failed: ". $conn->connect_error);
-// }
-
-// $sql = "select * from students where name like '%$search%'";
-
-// $result = $conn->query($sql);
-
-// if ($result->num_rows > 0){
-// while($row = $result->fetch_assoc() ){
-// 	echo $row["name"]."  ".$row["age"]."  ".$row["gender"]."<br>";
-// }
-// } else {
-// 	echo "0 records";
-// }
-
-// $conn->close();
-
-?>
