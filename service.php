38764015ea1371153fecf4bc424437e4cac93a37
LR-AJ <legalraastatech3@gmail.com>
2023-05-31 16:18:35 +0530

    update in service

--- service.php
+++ service.php
@@ -1,13 +1,13 @@
 <?php
 
 $search = $_GET['searchKey'];
-$domain = array("LLP Registration"=>"https://companyregistrationonline.in/llp-registration/", "Private Limited Company Registration"=>"https://companyregistrationonline.in/private-limited-company-registration/", "Nidhi Company Registeration"=>"https://companyregistrationonline.in/nidhi-company-registration/");
+$domain = array("LLP Registration"=>"/llp-registration/", "Private Limited Company Registration"=>"/private-limited-company-registration/", "Nidhi Company Registeration"=>"/nidhi-company-registration/","NBFC License"=>"/nbfc-registration/","Section 8 Company Registration"=>"/section-8-company-registration/","OPC Registration"=>"/opc-registration/");
 // $list = array('LLP Registration','NBFC License','Nidhi Company Registeration','Private Limited Company Registration','Section 8 Company Registration','OPC Registration');
 $list = ['LLP Registration','NBFC License','Nidhi Company Registeration','Private Limited Company Registration','Section 8 Company Registration','OPC Registration'];
 if(in_array($search,$list)){
   echo("check");
-  // header('Location:'.'https://companyregistrationonline.in/'. $domain[$search]);
-  header("Location: https://www.legalraasta.com/thanks/");
+  header('Location:'.'https://companyregistrationonline.in/'. $domain[$search]);
+  // header("Location: https://www.legalraasta.com/thanks/");
 
 }
 
